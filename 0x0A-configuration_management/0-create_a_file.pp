3b27ab962851835a168e68b804b6ad78f836404a
Juan Sebastian Llano <juanllano93@gmail.com>
2020-08-03 22:11:51 +0000

    Create a project 0x0A Configuration management and add zero  task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Equivalent resources:
 
 file { '/tmp/holberton':
-    path    => '/tmp/holberton',
     ensure  => file,
+    path    => '/tmp/holberton',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
