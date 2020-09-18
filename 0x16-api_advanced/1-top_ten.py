eed93af3b0cc514933277567c6a7d52ea6e91321
llanojs <juanllano93@gmail.com>
2020-09-18 10:13:10 -0500

    update shebang

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,4 +1,4 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 """
    Write a function that queries the Reddit API and returns the number of
    subscribers (not active users, total subscribers) for a given subreddit.
