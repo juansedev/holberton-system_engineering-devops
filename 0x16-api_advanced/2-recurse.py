d0cdce9edd06baddfad97681dbc745c953299e06
llanojs <juanllano93@gmail.com>
2020-09-18 00:08:38 -0500

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,8 +1,6 @@
 #!/user/bin/python3
 """
    Write a function that queries the Reddit API and returns the number of
-   subscribers (not active users, total subscribers) for a given subreddit.
-   f an invalid subreddit is given, the function should return 0.
 """
 
 import requests
@@ -14,7 +12,7 @@ def recurse(subreddit, hot_list=[], after=None):
         url = 'https://www.reddit.com/r/{}/hot.json?'.format(subreddit)
     else:
         url = 'https://www.reddit.com/r/{}/hot.json?after={}'\
-                .format(subreddit, after)
+            .format(subreddit, after)
     head = {"User-Agent": "Mozilla/5.0"}
     j_response = requests.get(url, headers=head).json()
 
